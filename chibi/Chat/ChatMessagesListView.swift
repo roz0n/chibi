3ac5f3e31c085111094fa23367377dfefd6e2a83
Arnold Rozon <arnold@rozon.org>
2023-10-10 15:18:48 -0700

    Refactored main views

---
+++ chibi/Chat/ChatMessagesListView.swift
@@ -0,0 +1,18 @@
+//
+//  ChatMessagesListView.swift
+//  chibi
+//
+//  Created by Arnaldo Rozon on 10/10/23.
+//
+
+import SwiftUI
+
+struct ChatMessagesListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ChatMessagesListView()
+}
---
+++ chibi/Chat/ChatNavigationToolbar.swift
@@ -0,0 +1,18 @@
+//
+//  ChatNavigationToolbar.swift
+//  chibi
+//
+//  Created by Arnaldo Rozon on 10/10/23.
+//
+
+import SwiftUI
+
+struct ChatNavigationToolbar: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ChatNavigationToolbar()
+}
